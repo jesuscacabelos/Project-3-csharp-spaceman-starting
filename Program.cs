99457487d4a9016ca11c2d00fce2520d9ced4cf7
Cacabelos <jesus.cacabelos@softwareone.com>
2021-09-29 13:07:23 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csharp_spaceman_starting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int j= 10;
+            string w = "nene";
+            bool car = true;
+        }
+    }
+}
